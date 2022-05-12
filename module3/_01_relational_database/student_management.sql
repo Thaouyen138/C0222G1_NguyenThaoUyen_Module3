5c64cddbb285ba52b8681b72b3b901c1daac5253
Thaouyen138 <96711178+Thaouyen138@users.noreply.github.com>
2022-05-12 11:31:34 +0700

    something

---
+++ module3/_01_relational_database/student_management.sql
@@ -0,0 +1,12 @@
+create database student_management;
+use student_management;
+
+create table class(
+id_class int,
+name_class varchar(50));
+
+create table teacher(
+id_teacher int,
+name_teacher varchar(50),
+age int, 
+country varchar(50));
