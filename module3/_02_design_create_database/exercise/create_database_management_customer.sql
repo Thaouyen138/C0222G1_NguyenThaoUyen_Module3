24757953c355be1a1bfe3fbeaba508e16e1071fb
Thaouyen138 <96711178+Thaouyen138@users.noreply.github.com>
2022-05-13 15:02:54 +0700

    something

---
+++ module3/_02_design_create_database/exercise/create_database_management_customer.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE management_customer;
+
+USE management_customer;
+
+CREATE TABLE product
+(
+	p_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	p_name VARCHAR(50),
+	p_price DOUBLE
+);
+    
+CREATE TABLE customer
+(
+    c_id INT PRIMARY KEY AUTO_INCREMENT,
+    c_name VARCHAR(30),
+    c_age int
+);
+
+CREATE TABLE order1
+(
+	o_id INT NOT NULL  PRIMARY KEY AUTO_INCREMENT,
+    c_id int ,
+    o_date DATETIME,
+    o_total_price DOUBLE,
+    FOREIGN KEY (c_id) REFERENCES customer(c_id)
+); 
+    
+CREATE TABLE order_detail
+(
+	o_id INT,
+    p_id INT,
+    od_qty INT,
+    PRIMARY KEY(o_id,p_id),
+    FOREIGN KEY(o_id)REFERENCES order1(o_id),
+    FOREIGN KEY(p_id)REFERENCES product(p_id)
+);
