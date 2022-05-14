878075d3412b8e4ce77496ceb42428e21873a464
Thaouyen138 <96711178+Thaouyen138@users.noreply.github.com>
2022-05-14 09:48:23 +0700

    something

---
+++ module3/_02_design_create_database/exercise/create_database_management_customer.sql
@@ -0,0 +1,72 @@
+CREATE DATABASE management_order;
+
+USE management_order;
+
+CREATE TABLE phone_number (
+    id_phone VARCHAR(30) PRIMARY KEY,
+    phone VARCHAR(13),
+    telephone VARCHAR(13)
+);
+
+CREATE TABLE product (
+    id_product VARCHAR(30) PRIMARY KEY,
+    name_product VARCHAR(30) NOT NULL,
+    address VARCHAR(50),
+    id_phone VARCHAR(30),
+    FOREIGN KEY (id_phone) REFERENCES phone_number (id_phone)
+);
+
+CREATE TABLE exports (
+    number_exports INT PRIMARY KEY,
+    date_exports DATE
+);
+
+CREATE TABLE imports (
+    number_import INT PRIMARY KEY,
+    date_import DATE
+);
+
+CREATE TABLE orders (
+    number_order INT PRIMARY KEY AUTO_INCREMENT,
+    date_order DATE,
+    id_product VARCHAR(30),
+    FOREIGN KEY (id_product) REFERENCES product (id_product)
+);
+
+CREATE TABLE facility (
+    id_facility INT PRIMARY KEY AUTO_INCREMENT,
+    name_facility VARCHAR(30)
+);
+
+CREATE TABLE order_facility (
+    number_order INT,
+    id_facility INT,
+    PRIMARY KEY (number_order , id_facility),
+    FOREIGN KEY (number_order) REFERENCES orders (number_order),
+    FOREIGN KEY (id_facility) REFERENCES facility (id_facility)
+);
+
+CREATE TABLE facility_export(
+	id_facility INT,
+    number_exports INT, 
+    price_products DOUBLE,
+    amount_exports INT,
+    PRIMARY KEY (id_facility,number_exports),
+	FOREIGN KEY (id_facility) REFERENCES facility (id_facility),
+	FOREIGN KEY (number_exports) REFERENCES exports (number_exports)
+    );
+    
+    
+CREATE TABLE facility_import(
+	id_facility INT,
+    number_imports INT, 
+    price_products DOUBLE,
+    amount_imports INT,
+    PRIMARY KEY (id_facility,number_imports),
+	FOREIGN KEY (id_facility) REFERENCES facility (id_facility),
+	FOREIGN KEY (number_imports) REFERENCES imports (number_import)
+    )
+    
+    
+
+
---
+++ module3/_case_study_database/furama_management.sql
@@ -0,0 +1,122 @@
+CREATE DATABASE furama_management;
+USE furama_management;
+
+CREATE TABLE vi_tri
+(
+    id_vi_tri INT PRIMARY KEY AUTO_INCREMENT,
+    ten_vi_tri VARCHAR(45)
+);
+
+CREATE TABLE trinh_do
+(
+    id_trinh_do INT PRIMARY KEY AUTO_INCREMENT,
+    ten_trinh_do VARCHAR(45)
+);
+
+CREATE TABLE bo_phan
+( 
+	id_bo_phan INT PRIMARY KEY AUTO_INCREMENT,
+	ten_bo_phan VARCHAR(45)
+);
+CREATE TABLE nhan_vien
+(
+	id_nhan_vien INT PRIMARY KEY AUTO_INCREMENT,
+    ho_va_ten VARCHAR(45),
+    ngay_sinh DATE,
+    so_cmnd VARCHAR(45),
+    luong DOUBLE,
+    so_dien_thoai VARCHAR(45),
+    email VARCHAR(45),
+    dia_chi VARCHAR(45),
+	id_vi_tri INT,
+	id_trinh_do INT,
+    id_bo_phan INT,
+    FOREIGN KEY (id_vi_tri) REFERENCES vi_tri(id_vi_tri),
+    FOREIGN KEY (id_trinh_do) REFERENCES trinh_do(id_trinh_do),
+    FOREIGN KEY (id_bo_phan) REFERENCES bo_phan(id_bo_phan)
+);
+CREATE TABLE loai_khach
+( 
+	id_loai_khach INT PRIMARY KEY AUTO_INCREMENT,
+	ten_loai_khach VARCHAR(45)
+);
+
+CREATE TABLE khach_hang
+(
+	id_khach_hang INT PRIMARY KEY AUTO_INCREMENT,
+	id_loai_khach INT,
+    FOREIGN KEY (id_loai_khach) REFERENCES loai_khach(id_loai_khach),
+	ho_va_ten VARCHAR(45),
+    ngay_sinh DATE,
+	gioi_tinh BIT DEFAULT 1,
+	so_cmnd VARCHAR(45),
+    so_dien_thoai VARCHAR(45),
+    email VARCHAR(45),
+    dia_chi VARCHAR(45)
+);
+
+CREATE TABLE dich_vu_di_kem
+(
+	id_dich_vu_di_kem INT PRIMARY KEY AUTO_INCREMENT,
+	ten_dich_vu VARCHAR(45),
+	gia DOUBLE,
+	don_vi VARCHAR(45),
+	trang_thai VARCHAR(45)
+);
+CREATE TABLE kieu_thue
+(
+id_kieu_thue INT PRIMARY KEY AUTO_INCREMENT,
+ten_kieu_thue VARCHAR(45)
+);
+
+CREATE TABLE loai_dich_vu
+(
+id_loai_dich_vu INT PRIMARY KEY AUTO_INCREMENT,
+ten_loai_dich_vu VARCHAR(45)
+);
+
+CREATE TABLE dich_vu
+(
+id_dich_vu INT PRIMARY KEY AUTO_INCREMENT,
+ten_dich_vu VARCHAR(45),
+dien_tich INT, 
+chi_phi_thue DOUBLE,
+so_nguoi_toi_da INT,
+id_kieu_thue INT,
+FOREIGN KEY (id_kieu_thue) REFERENCES kieu_thue(id_kieu_thue),
+id_loai_dich_vu INT,
+FOREIGN KEY (id_loai_dich_vu) REFERENCES loai_dich_vu(id_loai_dich_vu),
+tieu_chuan_phong VARCHAR(45),
+mo_ta_tien_nghi_khac VARCHAR(45),
+dien_dich_ho_boi DOUBLE,
+so_tang INT
+
+);
+CREATE TABLE hop_dong
+(
+id_hop_dong INT PRIMARY KEY AUTO_INCREMENT,
+ngay_lam_hop_dong DATETIME,
+ngay_ket_thuc DATETIME,
+tien_dat_coc DOUBLE,
+id_nhan_vien INT, 
+id_khach_hang INT,
+id_dich_vu INT,
+FOREIGN KEY (id_nhan_vien) REFERENCES nhan_vien(id_nhan_vien),
+FOREIGN KEY (id_khach_hang) REFERENCES khach_hang(id_khach_hang),
+FOREIGN KEY (id_dich_vu) REFERENCES dich_vu(id_dich_vu)
+);
+
+CREATE TABLE hop_dong_chi_tiet
+(
+	id_hop_dong_chi_tiet INT PRIMARY KEY AUTO_INCREMENT,
+    id_hop_dong INT ,
+    id_dich_vu_di_kem INT,
+    FOREIGN KEY (id_hop_dong) REFERENCES hop_dong(id_hop_dong),
+    FOREIGN KEY (id_dich_vu_di_kem) REFERENCES dich_vu_di_kem(id_dich_vu_di_kem),
+    so_luong INT
+);
+
+    
+
+    
+
